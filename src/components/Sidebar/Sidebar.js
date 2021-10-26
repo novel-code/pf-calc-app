b50e5b6c117307134cbacb6f3db1d117d23c9102
novel-code <novelcode0@gmail.com>
2021-10-26 18:33:29 +0530

    formatted

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,33 +1,42 @@
 import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { AiOutlineUserAdd } from "react-icons/ai";
-import logo from '../../assets/logo.jpg'
-import styleSidebar from './sidebar.module.css'
-import {BsTable} from 'react-icons/bs'
+import logo from "../../assets/logo.jpg";
+import styleSidebar from "./sidebar.module.css";
+import { BsTable } from "react-icons/bs";
 const Sidebar = function () {
-    const [btnActive , setBtnActive] = useState(false);
+  const [btnActive, setBtnActive] = useState(false);
 
+  return (
+    <div className={styleSidebar.sidebar}>
+      <div className={styleSidebar.logoDiv}>
+        <img className={styleSidebar.logoImg} src={logo}></img>
+      </div>
+      <div className={styleSidebar.linkDiv}>
+        <NavLink
+          className={styleSidebar.linkNav}
+          activeClassName={styleSidebar.selected}
+          to={(location) => ({ ...location, pathname: "/add" })}
+        >
+          <AiOutlineUserAdd
+            style={{ fontSize: "1.3rem", marginRight: "0.5rem" }}
+          ></AiOutlineUserAdd>{" "}
+          Add
+        </NavLink>
 
-    return (
-        <div className={styleSidebar.sidebar}>
-            <div className={styleSidebar.logoDiv}>
-
-            <img className={styleSidebar.logoImg}  src={logo}></img>
-            </div>
-            <div className={styleSidebar.linkDiv}>
-
-            <NavLink className={styleSidebar.linkNav} activeClassName={styleSidebar.selected} to={location => ({ ...location, pathname: "/add" })} ><AiOutlineUserAdd style={{fontSize: "1.3rem", marginRight: "0.5rem"}}></AiOutlineUserAdd > Add</NavLink>
-
-        
-            <NavLink className={styleSidebar.linkNav} activeClassName={styleSidebar.selected} to="/list" ><BsTable style={{fontSize: "1.3rem", marginRight: "0.5rem"}} ></BsTable> List</NavLink>
-               
-                
-
-
-            </div>
-          
-        </div>
-    )
-}
+        <NavLink
+          className={styleSidebar.linkNav}
+          activeClassName={styleSidebar.selected}
+          to="/list"
+        >
+          <BsTable
+            style={{ fontSize: "1.3rem", marginRight: "0.5rem" }}
+          ></BsTable>{" "}
+          List
+        </NavLink>
+      </div>
+    </div>
+  );
+};
 
 export default Sidebar;
